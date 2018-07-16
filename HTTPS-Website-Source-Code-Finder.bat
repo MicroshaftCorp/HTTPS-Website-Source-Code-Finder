3516dcb6366b755c0480fbaaf218be9f4dfd7040
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-16 12:38:18 -0700

    Update HTTPS-Website-Source-Code-Finder.bat

--- HTTPS-Website-Source-Code-Finder.bat
+++ HTTPS-Website-Source-Code-Finder.bat
@@ -1,2 +1,24 @@
 @echo off
-title HTTPS Website-Source-Code Finder
+title HTTPS Website Source Code Finder
+echo Welcome To HTTPS-Website-Source-Code-Finder!
+echo (note:first we'll ask for the website name and then the domain.)
+pause
+cls
+echo Domain Site Name?
+set /p SiteName=
+cls
+echo domain?
+set /p domain=
+cls
+echo Compiling.....
+timeout 4
+cls
+echo Adding WWWdot.....
+timeout 4
+cls
+echo Opening.....
+timeout 2
+start view-source:https://%SiteName%.%domain%
+echo Progess Complete!
+:9
+[]vh
